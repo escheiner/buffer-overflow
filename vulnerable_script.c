b477601264f1cbe824d2fa5f3acae3aa942b483e
Esty Scheiner <stscheiner11@gmail.com>
2020-10-28 18:17:43 -0600

    Update and rename meet.c to vulnerable_script.c

--- vulnerable_script.c
+++ vulnerable_script.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 void greeting(char *temp1,char *temp2){ // greeting function to say hello
-    char name[400]; // string variable to hold the name
+    char name[401]; // string variable to hold the name
     strcpy(name, temp2); // copy the function argument to name
     printf("Hello %s %s\n", temp1, name); //print out the greeting
 }
